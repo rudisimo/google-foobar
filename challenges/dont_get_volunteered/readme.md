78d3ee4b3c253c9c604db04accf3599a4b0d9e05
Rodolfo Puig <rodolfo@puig.io>
2017-09-29 15:23:34 -0400

    Updating submitted 3rd challenge

--- challenges/dont_get_volunteered/readme.md
+++ challenges/dont_get_volunteered/readme.md
@@ -5,23 +5,16 @@ As a henchman on Commander Lambda's space station, you're expected to be resourc
 
 To help yourself get to and from your bunk every day, write a function called answer(src, dest) which takes in two parameters: the source square, on which you start, and the destination square, which is where you need to land to solve the puzzle.  The function should return an integer representing the smallest number of moves it will take for you to travel from the source square to the destination square using a chess knight's moves (that is, two squares in any direction immediately followed by one square perpendicular to that direction, or vice versa, in an "L" shape).  Both the source and destination squares will be an integer between 0 and 63, inclusive, and are numbered like the example chessboard below:
 
--------------------------
-| 0| 1| 2| 3| 4| 5| 6| 7|
--------------------------
-| 8| 9|10|11|12|13|14|15|
--------------------------
-|16|17|18|19|20|21|22|23|
--------------------------
-|24|25|26|27|28|29|30|31|
--------------------------
-|32|33|34|35|36|37|38|39|
--------------------------
-|40|41|42|43|44|45|46|47|
--------------------------
-|48|49|50|51|52|53|54|55|
--------------------------
-|56|57|58|59|60|61|62|63|
--------------------------
+| - | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 |
+|---|---|---|---|---|---|---|---|---|
+| **1** | 0| 1| 2| 3| 4| 5| 6| 7|
+| **2** | 8| 9|10|11|12|13|14|15|
+| **3** |16|17|18|19|20|21|22|23|
+| **4** |24|25|26|27|28|29|30|31|
+| **5** |32|33|34|35|36|37|38|39|
+| **6** |40|41|42|43|44|45|46|47|
+| **7** |48|49|50|51|52|53|54|55|
+| **8** |56|57|58|59|60|61|62|63|
 
 Languages
 =========
@@ -33,15 +26,19 @@ Test cases
 ==========
 
 Inputs:
+
     (int) src = 19
     (int) dest = 36
 Output:
+
     (int) 1
 
 Inputs:
+
     (int) src = 0
     (int) dest = 1
 Output:
+
     (int) 3
 
 Use verify [file] to test your solution and see how it does. When you are finished editing your code, use submit [file] to submit your answer. If your solution passes the test cases, it will be removed from your home folder.
--- runtime/dont_get_volunteered/solution.py
+++ runtime/dont_get_volunteered/solution.py
@@ -1,77 +1,87 @@
-# define chessboard knight movements
-BOARD_SIZE = 8
+import heapq
+from collections import defaultdict
 
-def calculate_possible_moves():
-    row = [2, 2, -2, -2, 1, 1, -1, -1]
-    column = [-1, 1, 1, -1, 2, -2, 2, -2]
-    deltas = zip(row, column)
+# define chessboard parameters
+BOARD_SIZE      = 8
+BOARD_MOVEMENTS = [(1, 2), (1, -2), (-1, 2), (-1, -2),
+                   (2, 1), (2, -1), (-2, 1), (-2, -1)]
 
-    return [
-        [6, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 6],
-        [5, 4, 5, 4, 3, 4, 3, 4, 3, 4, 3, 4, 5, 4, 5],
-        [4, 5, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 5, 4],
-        [5, 4, 3, 4, 3, 2, 3, 2, 3, 2, 3, 4, 3, 4, 5],
-        [4, 3, 4, 3, 2, 3, 2, 3, 2, 3, 2, 3, 4, 3, 4],
-        [5, 4, 3, 2, 3, 4, 1, 2, 1, 4, 3, 2, 3, 4, 5],
-        [4, 3, 4, 3, 2, 1, 2, 3, 2, 1, 2, 3, 4, 3, 4],
-        [5, 4, 3, 2, 3, 2, 3, 0, 3, 2, 3, 2, 3, 4, 5],
-        [4, 3, 4, 3, 2, 1, 2, 3, 2, 1, 2, 3, 4, 3, 4],
-        [5, 4, 3, 2, 3, 4, 1, 2, 1, 4, 3, 2, 3, 4, 5],
-        [4, 3, 4, 3, 2, 3, 2, 3, 2, 3, 2, 3, 4, 3, 4],
-        [5, 4, 3, 4, 3, 2, 3, 2, 3, 2, 3, 4, 3, 4, 5],
-        [4, 5, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 5, 4],
-        [5, 4, 5, 4, 3, 4, 3, 4, 3, 4, 3, 4, 5, 4, 5],
-        [6, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 6]
-    ]
+class Node:
+    def __init__(self, x, y, distance=0):
+        self.x = x
+        self.y = y
+        self.distance = distance
 
-def calculate_possible_moves(start, finish):
-    board = [[0 for y in xrange(BOARD_SIZE)] for x in xrange(BOARD_SIZE)]
-    #print(board)
+    def __hash__(self):
+        return hash(tuple(self))
 
-    queue = [start]
-    offsets = [(1, 2), (1, -2), (-1, 2), (-1, -2),
-               (2, 1), (2, -1), (-2, 1), (-2, -1)]
+    def __iter__(self):
+        for i in [self.x, self.y]:
+            yield i
 
-    while len(queue):
-        current = queue.pop(0)
-        if current == finish:
-            return 
+    def __eq__(self, node):
+        return self.x == node.x and self.y == node.y
 
-        for offset in offsets:
-            step = tuple(x + y for x, y in zip(current, offset))
-            if step[0] > 0 and step[0] < finish:
-                continue
-            if step[1] > finish[1]:
-                continue
-            if step[0] < 1:
-                continue
-            if step[1] < 1:
-                continue
-            if 
+    def __lt__(self, other):
+        return self.x < node.x or (self.x == node.x and self.y < node.y)
 
-        #    if 0 < move[0] and 0 < move[1] < 8:
-        #        yield move
+    def __str__(self):
+        return "<Node (x=%d, y=%d, d=%.02f)>" % (self.x, self.y, self.distance)
 
-def calculate_position_coordinates(position):
-    return int(position % BOARD_SIZE) + 1, int(int(position / BOARD_SIZE) + 1)
+    def __repr__(self):
+        return self.__str__()
+
+class Board:
+    def __init__(self, size, movements):
+        self.size = size
+        self.movements = movements
+
+    def node(self, position):
+        """ Generate Node for given position """
+        return Node(int(position % self.size) + 1, int(position / self.size) + 1)
+
+    def valid(self, x, y):
+        return True if x >= 0 < self.size and y >= 0 < self.size else False
+
+    def distance(self, start, end):
+        """ Find shortest distance using BFS """
+        queue = []
+        queue.append(start)
+        visited = {}
+
+        # run until queue is empty
+        while queue:
+            # grab first node in queue
+            node = queue.pop(0)
+
+            # return when destination is reached
+            if node == end:
+                return node.distance
+
+            # process nodes that have not been visited
+            if node not in visited:
+                # mark node as visited
+                visited[node] = True
+
+                # validate piece movement
+                for offset in self.movements:
+                    x, y = list(tuple(x + y for x, y in zip(tuple(node), offset)))
+
+                    # queue valid moves only
+                    if self.valid(x, y):
+                        queue.append(Node(x, y, node.distance + 1))
+
+        return float('inf')
 
 def answer(src, dest):
-    distance = {}
-    position = {}
+    # create chessboard graph
+    chessboard = Board(size = BOARD_SIZE, movements = BOARD_MOVEMENTS)
 
-    start = calculate_position_coordinates(src)
-    finish = calculate_position_coordinates(dest)
-    print(start)
-    print(finish)
+    # create coordinates
+    start = chessboard.node(src)
+    end = chessboard.node(dest)
 
-    queue = [start]
+    # calculate distance between given positions
+    distance = chessboard.distance(start, end)
 
-    moves = calculate_possible_moves(start, finish)
-
-    print(moves)
-
-    maximum = (7, 7)
-    distance = tuple(x - y for x, y in zip(start, finish))
-    position = tuple(x + y for x, y in zip(distance, maximum))
-
-    return moves[position[0]][position[1]]
+    return distance
