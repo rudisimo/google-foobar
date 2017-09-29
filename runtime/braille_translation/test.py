65ca93be3fcbb19369716d519db4c240abd68960
Rodolfo Puig <rodolfo@puig.io>
2017-09-28 20:03:16 -0400

    Restructuring project

--- runtime/braille_translation/test.py
+++ runtime/braille_translation/test.py
@@ -1,22 +1,22 @@
 import unittest
-from . import answer
+from . import solution
 
-class TestGoogleSolution(unittest.TestCase):
+class TestChallengeSolution(unittest.TestCase):
     def test_translate_simple(self):
         self.assertEqual(
-            answer('code'),
+            solution.answer('code'),
             '100100101010100110100010'
         )
 
     def test_translate_uppercase(self):
         self.assertEqual(
-            answer('Braille'),
+            solution.answer('Braille'),
             '000001110000111010100000010100111000111000100010'
         )
 
     def test_translate_lazy_dog(self):
         self.assertEqual(
-            answer('The quick brown fox jumped over the lazy dog'),
+            solution.answer('The quick brown fox jumped over the lazy dog'),
             '000001011110110010100010000000111110101001010100100100101000000000110000111010101010010111101110000000110100101010101101000000010110101001101100111100100010100110000000101010111001100010111010000000011110110010100010000000111000100000101011101111000000100110101010110110'
         )
 
