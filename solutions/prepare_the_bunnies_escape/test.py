08506ee86d4169b7c366e08e7597a00e988da0cc
Rodolfo Puig <rodolfo@puig.io>
2017-09-30 19:04:05 -0400

    Adding unit test for the latest challenge

--- solutions/prepare_the_bunnies_escape/test.py
+++ solutions/prepare_the_bunnies_escape/test.py
@@ -2,4 +2,14 @@ import unittest
 from . import solution
 
 class TestCase(unittest.TestCase):
-    pass
+    def test_case_1(self):
+        self.assertEqual(
+            solution.answer([[0, 1, 1, 0], [0, 0, 0, 1], [1, 1, 0, 0], [1, 1, 1, 0]]),
+            7
+        )
+
+    def test_case_2(self):
+        self.assertEqual(
+            solution.answer([[0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0]]),
+            11
+        )
